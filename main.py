cc56b2614ab5e86575235f54b7470e52c2587143
TalB <tal.brilant@gmail.com>
2023-02-28 13:04:09 +0200

    Includes chatGPT API

--- main.py
+++ main.py
@@ -22,38 +22,45 @@ def read_string(text):
 
 
 # connection with openAI API
-openai.api_key = "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"
-
+openai.api_key = "MY_API_KEY"
+openai.api_key = "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"
+# setup audio and speech recognizer instance
 r = sr.Recognizer()
 
+
 with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source)  # starts and stops recording by default silence time
+    with open('name.txt') as f:
+        while True:
+            audio = r.listen(source)  # starts and stops recording by default silence time
 
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    generated_text = r.recognize_google(audio)
+            try:
+                # for testing purposes, we're just using the default API key
+                # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
 
-    print(f"The program thinks you said: \n>>>\t\t {generated_text}")
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-    read_string("I could not understand you")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    read_string("Error, restart the app please")
+                # generating the text from recording
+                generated_text = r.recognize_google(audio)
+                print(f"The program thinks you said: \n>>>\t\t {generated_text}")
 
-# Sending the received text to chatGPT
-response = openai.Completion.create(
-  model="text-davinci-003",  # chatGPT model
-  prompt=generated_text,
-  temperature=0.3,  # creativeness
-  max_tokens=150,
-  frequency_penalty=0.0,  # recurrence of sentences
-  presence_penalty=0.6,  # less repetitiveness
-)
+                # if the user called the AI name (inside 'name.txt')
+                if f"hey {f.readlines()}" in generated_text:
+                    read_string("Hey. ask me anything!")
 
-print(response.choices[0].text)
-read_string(response.choices[0].text)
+                    # Sending the received text to chatGPT
+                    response = openai.Completion.create(
+                        model="text-davinci-003",  # chatGPT model
+                        prompt=generated_text,
+                        temperature=0.3,  # creativeness
+                        max_tokens=150,
+                        frequency_penalty=0.0,  # recurrence of sentences
+                        presence_penalty=0.6,  # less repetitiveness
+                    )
 
+                    print(response.choices[0].text)
+                    read_string(response.choices[0].text)
+
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+                read_string("I could not understand you")
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                read_string("Error, restart the app please")
